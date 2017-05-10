69e63f704c487194bd25c2c514b9d36257968347
julianjing <julianjing@aliyun>
2017-05-10 16:54:56 +0800

    tracer

---
+++ web/trace/tracer.go
@@ -0,0 +1,36 @@
+// Package trace provides ...
+package trace
+
+import (
+	"fmt"
+	"io"
+)
+
+// Tracer is the interface that describes an object capable of
+// tracing events throughout code.
+type Tracer interface {
+	Trace(...interface{})
+}
+
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
+}
+
+type tracer struct {
+	out io.Writer
+}
+
+func (t *tracer) Trace(a ...interface{}) {
+	fmt.Fprint(t.out, a...)
+	fmt.Fprintln(t.out)
+}
+
+func Off() Tracer {
+	return &nilTracer{}
+}
+
+type nilTracer struct{}
+
+func (n nilTracer) Trace(a ...interface{}) {
+
+}
