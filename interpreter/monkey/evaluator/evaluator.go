1762f9a1a05fb4e82ae53bafefdea8e0b70c04b0
Tim Jing <tim@evolveconsulting.com.hk>
2018-05-02 18:11:46 +0800

    interpreter

---
+++ interpreter/monkey/evaluator/evaluator.go
@@ -0,0 +1,41 @@
+package evaluator
+
+import (
+	"github.com/timpointer/golang-demo/interpreter/monkey/ast"
+	"github.com/timpointer/golang-demo/interpreter/monkey/object"
+)
+
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
+func Eval(node ast.Node) object.Object {
+
+	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
+	}
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+	for _, stmt := range stmts {
+		result = Eval(stmt)
+	}
+	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
+}
--- interpreter/monkey/repl/repl.go
+++ interpreter/monkey/repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/timpointer/golang-demo/interpreter/monkey/evaluator"
 	"github.com/timpointer/golang-demo/interpreter/monkey/lexer"
 	"github.com/timpointer/golang-demo/interpreter/monkey/parser"
 )
@@ -30,9 +31,11 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
